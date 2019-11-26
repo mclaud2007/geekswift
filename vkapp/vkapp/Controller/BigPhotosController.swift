f7b08a268de0e632f2771477e1ea11081cb7b99c
mclaud2007 <grigoriy@martushin.ru>
2019-11-26 22:52:35 +0300

    Урок 10: Добавил интерактивную анимацию перелистывания больших фотографий друзей, а также попробовал убрать проблемы с анимацией при быстром перелистывании фотографий (есть подозрение, что у меня получилось :)).

--- vkapp/vkapp/Controller/BigPhotosController.swift
+++ vkapp/vkapp/Controller/BigPhotosController.swift
@@ -16,6 +16,19 @@ class BigPhotosController: UIViewController {
     // Массив фотографий выбранного пользователя (должен прийти из предыдущего окна или выведем фото notfound)
     var PhotosLists = [UIImage(named: "photonotfound")]
     var CurrentImageNumber: Int = 0
+    var animationHasFinished: Bool = true
+    
+    // Направление свайпа
+    enum panDirectionEnum {
+        case left
+        case right
+        case none
+    }
+    
+    // Присвоем значение по-умолчанию - потом поменяем
+    var panDirect: panDirectionEnum = .left
+    
+    var panInteractiveAnimator: UIViewPropertyAnimator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +41,8 @@ class BigPhotosController: UIViewController {
             self.BigPhotoImageView.image = PhotosLists[0]
         }
     
-        let swipeRecognizerLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeCatch(_:)))
-        swipeRecognizerLeft.direction = .left
-        self.view.addGestureRecognizer(swipeRecognizerLeft)
-        
-        let swipeRecognizerRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeCatch(_:)))
-        swipeRecognizerRight.direction = .right
-        self.view.addGestureRecognizer(swipeRecognizerRight)
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panCatch(_:)))
+        self.view.addGestureRecognizer(panRecognizer)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,80 +53,145 @@ class BigPhotosController: UIViewController {
         self.BigPhotoImageViewTmp.isHidden = true
     }
     
-    @objc func swipeCatch(_ sender : UISwipeGestureRecognizer){
-        let PhotosCount = PhotosLists.count - 1
-
-        if sender.direction == .right {
-            if self.CurrentImageNumber > 0 {
-                self.CurrentImageNumber -= 1
+    @objc func panCatch( _ sender: UIPanGestureRecognizer){
+        switch sender.state {
+        case .began:
+            let transition = sender.translation(in: self.view)
+            
+            // Определяем направление движения
+            if transition.x < 0 {
+                self.panDirect = .left
+            } else if transition.x > 0 {
+                self.panDirect = .right
             } else {
-                self.CurrentImageNumber = PhotosCount
+                self.panDirect = .none
             }
-        } else if sender.direction == .left {
-            if (self.CurrentImageNumber + 1) != PhotosCount && (self.CurrentImageNumber + 1) <= PhotosCount {
-                self.CurrentImageNumber += 1
+            
+            if self.panDirect != .none {
+                self.startAnimation()
+            }
+            
+        case .changed:
+            guard let propertyAnimator = self.panInteractiveAnimator else { return }
+            
+            switch self.panDirect {
+            case .right:
+                let percent = min(max(0, sender.translation(in: view).x / 200), 1)
+                propertyAnimator.fractionComplete = percent
+            case .left:
+                let percent = min(max(0, -sender.translation(in: view).x / 200), 1)
+                propertyAnimator.fractionComplete = percent
+            case .none:
+                let transition = sender.translation(in: self.view)
+                
+                // Определяем направление движения
+                if transition.x < 0 {
+                    self.panDirect = .left
+                    self.startAnimation()
+                } else if transition.x > 0 {
+                    self.panDirect = .right
+                    self.startAnimation()
+                } else {
+                    self.panDirect = .none
+                }
+            }
+            
+        case .ended:
+            guard let propertyAnimator = self.panInteractiveAnimator else { return }
+            
+            if propertyAnimator.fractionComplete > 0.33 {
+                propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0.5)
             } else {
-                self.CurrentImageNumber = 0
+                propertyAnimator.isReversed = true
+                propertyAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0.5)
             }
+            
+        default:
+            break
         }
-        
-        self.startAnimation(direction: sender.direction)
     }
     
-    private func startAnimation(direction: UISwipeGestureRecognizer.Direction = .left) {
-        // Новую фотографию загружаем во второй ImageView, который будет выезжать
-        if let CurrentImage = self.PhotosLists[self.CurrentImageNumber] {
-            self.BigPhotoImageViewTmp.image = CurrentImage
-        } else {
-            self.BigPhotoImageViewTmp.image = UIImage(named: "photonotfound")!
+    private func startAnimation(){
+        if self.animationHasFinished == true {
+            // Стартуя анимацию пока она не завершится, новую запускать нельзя
+            self.animationHasFinished = false
+            
+            // Получим текущую фотографию в зависимости от того в какую сторону свайпим
+            let CurrentImageNum = self.getCurrentPhotoNum()
+            
+            // Новую фотографию загружаем во второй ImageView, который будет выезжать
+            if let CurrentImage = self.PhotosLists[CurrentImageNum] {
+                self.BigPhotoImageViewTmp.image = CurrentImage
+            } else {
+                self.BigPhotoImageViewTmp.image = UIImage(named: "photonotfound")!
+            }
+            
+            // Скрываем фотографию за краем экрана
+            if self.panDirect == .left {
+                self.BigPhotoImageViewTmp.transform = CGAffineTransform(translationX: -self.view.bounds.width, y: 0).concatenating(CGAffineTransform(scaleX: 0.5, y: 0.5))
+            } else {
+                self.BigPhotoImageViewTmp.transform = CGAffineTransform(translationX: self.view.bounds.width, y: 0).concatenating(CGAffineTransform(scaleX: 0.5, y: 0.5))
+            }
+            
+            self.BigPhotoImageViewTmp.layer.zPosition = 100
+            self.BigPhotoImageViewTmp.isHidden = false
+            
+            // Создаем универсальную анимацию
+            panInteractiveAnimator = UIViewPropertyAnimator(duration: 2, curve: .easeInOut, animations: {
+                if self.panDirect == .left {
+                    self.BigPhotoImageView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5).concatenating(CGAffineTransform(translationX: -2 * self.view.bounds.width, y: 0))
+                    self.BigPhotoImageViewTmp.transform = .identity
+                    
+                } else {
+                    self.BigPhotoImageView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5).concatenating(CGAffineTransform(translationX: 2 * self.view.bounds.width, y: 0))
+                    self.BigPhotoImageViewTmp.transform = .identity
+                }
+            })
+            
+            // Код который запуститься по окончании анимации
+            panInteractiveAnimator.addCompletion { position in
+                // Если анимация достигла конца надо заменить картинку
+                if position == .end {
+                    self.BigPhotoImageView.image = self.BigPhotoImageViewTmp.image
+                    self.BigPhotoImageView.transform = .identity
+                    self.BigPhotoImageView.layer.zPosition = 100
+                    self.CurrentImageNumber = self.getCurrentPhotoNum()
+                }
+                
+                // В конце или в начале надо вернуть временное изображение в начало
+                if position == .start || position == .end {
+                    // Вернем все настройки временного фото в начало
+                    self.BigPhotoImageViewTmp.image = nil
+                    self.BigPhotoImageViewTmp.layer.zPosition = 10
+                    self.BigPhotoImageViewTmp.center.x -= self.view.bounds.width
+                }
+                
+                // И поставить признак того что анимация закончилась
+                self.animationHasFinished = true
+            }
+            
+            panInteractiveAnimator.startAnimation()
         }
+    }
+    
+    private func getCurrentPhotoNum() -> Int {
+        let PhotosCount = PhotosLists.count - 1
+        var retNumber = self.CurrentImageNumber
 
-        // Показваем слой и вытаскиваем его на передний план
-        if direction == .left {
-            self.BigPhotoImageViewTmp.center.x -= self.view.bounds.width
-        } else {
-            self.BigPhotoImageViewTmp.center.x += self.view.bounds.width
+        if self.panDirect == .right {
+            if self.CurrentImageNumber > 0 {
+                retNumber = self.CurrentImageNumber - 1
+            } else {
+                retNumber = PhotosCount
+            }
+        } else if self.panDirect == .left {
+            if (self.CurrentImageNumber + 1) <= PhotosCount {
+                retNumber = self.CurrentImageNumber + 1
+            } else {
+                retNumber = 0
+            }
         }
         
-        self.BigPhotoImageViewTmp.layer.zPosition = 100
-        self.BigPhotoImageViewTmp.isHidden = false
-        
-        // Запускаем анимации
-        UIView.animateKeyframes(withDuration: 2, delay: 0, options: [],
-                                animations: {
-                                    // Уменьшаем оригинал
-                                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-                                        self.BigPhotoImageView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-                                        self.BigPhotoImageView.layer.opacity = 0.3
-                                        self.BigPhotoImageView.layer.zPosition = 10
-                                    }
-                                    // Отправляем его за пределы экрана
-                                    UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.45) {
-                                        if direction == .left {
-                                            self.BigPhotoImageView.center.x -= self.view.bounds.width
-                                        } else {
-                                            self.BigPhotoImageView.center.x += self.view.bounds.width
-                                        }
-                                    }
-                                    // И выдвигаем чуть позже из-за края экрана новую картинку
-                                    UIView.addKeyframe(withRelativeStartTime: 0.35, relativeDuration: 0.5) {
-                                        if direction == .left {
-                                            self.BigPhotoImageViewTmp.center.x += self.view.bounds.width
-                                        } else {
-                                            self.BigPhotoImageViewTmp.center.x -= self.view.bounds.width
-                                        }
-                                    }
-                                }, completion: { _ in
-                                    self.BigPhotoImageView.image = self.BigPhotoImageViewTmp.image
-                                    self.BigPhotoImageView.transform = .identity
-                                    self.BigPhotoImageView.layer.opacity = 1
-                                    self.BigPhotoImageView.layer.zPosition = 100
-                                    
-                                    self.BigPhotoImageViewTmp.center.x = 0
-                                    self.BigPhotoImageViewTmp.transform = .identity
-                                    self.BigPhotoImageViewTmp.layer.opacity = 1
-                                    self.BigPhotoImageViewTmp.layer.zPosition = 10
-                                    self.BigPhotoImageViewTmp.isHidden = true
-                                })
+        return retNumber
     }
 }
